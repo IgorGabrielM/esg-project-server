c00ffd26bd19ee5d9f61f02115db39aea2cb495f
Nailton Vital <nailtonvital35@gmail.com>
2025-05-21 20:38:55 -0300

    feat: implement user management endpoints for findAll, findOne, update, and remove functionalities

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -4,10 +4,15 @@ import {
   Post,
   Body,
   ValidationPipe,
+  Param,
+  Request,
+  Patch,
+  Delete,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { Public } from 'src/auth/decorators/public.decorator';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users')
 export class UsersController {
@@ -24,4 +29,46 @@ export class UsersController {
   async findAllByRanking() {
     return this.usersService.findAllByRank();
   }
+
+  @Get()
+  async findAll() {
+    try {
+      return await this.usersService.findAll();
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string) {
+    try {
+      return await this.usersService.findOneById(+id);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  @Patch(':id')
+  async update(
+    @Param('id') id: string,
+    @Body() updateUserDto: UpdateUserDto,
+    @Request() req,
+  ) {
+    console.log(req.user);
+    try {
+      return await this.usersService.update(+id, updateUserDto);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  @Delete(':id')
+  async remove(@Param('id') id: string) {
+    try {
+      return await this.usersService.remove(+id);
+    } catch (error) {
+      throw error;
+    }
+  }
+
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,16 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import * as bcrypt from 'bcrypt';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(User)
     private userRepository: Repository<User>,
-  ) {}
+  ) { }
 
   async create(createUserDto: CreateUserDto) {
     const hashedPassword = await bcrypt.hash(createUserDto.password, 10);
@@ -26,6 +27,16 @@ export class UsersService {
       .getRawMany();
   }
 
+  async findAll() {
+    try {
+      return await this.userRepository.find({
+        relations: { posts: true },
+      });
+    } catch (error) {
+      throw error;
+    }
+  }
+
   // Find user by email for authentication
   async findOne(email: string): Promise<User> {
     return await this.userRepository.findOne({ where: { email } });
@@ -36,11 +47,43 @@ export class UsersService {
     return await this.userRepository.findOne({ where: { idUser: id } });
   }
 
+  async update(userId: number, updateUserDto: UpdateUserDto): Promise<User> {
+    try {
+      const existingUser = await this.userRepository.findOne({
+        where: { idUser: userId },
+      });
+
+      if (!existingUser) {
+        throw new NotFoundException('User not found for update.');
+      }
+
+      const { password, ...userData } = updateUserDto;
+
+      Object.assign(existingUser, userData);
+
+      if (password) {
+        existingUser.password = await bcrypt.hash(password, 10);
+      }
+
+
+
+      return await this.userRepository.save(existingUser);
+    } catch (error) {
+      throw error;
+    }
+  }
+
   async addPoints(id: number, points: number) {
     const user = await this.userRepository.findOne({ where: { idUser: id } });
     if (!user) throw new Error('User not found');
     user.points += points;
     return await this.userRepository.save(user);
+  }
+  
+  async remove(id: number) {
+    const user = await this.userRepository.findOne({ where: { idUser: id } });
+    if (!user) throw new NotFoundException('User not found for deletion.');
+    return await this.userRepository.remove(user);
   }
 
   // Update and remove operations are not needed in simplified version
